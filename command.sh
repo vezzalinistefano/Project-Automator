4f6d1d6281a0df33595b0be55856b117ae576687
Vezzalini Stefano <stefano@MacBook-Air-di-Vezzalini.local>
2019-10-16 11:34:24 +0200

    Small updates

--- command.sh
+++ command.sh
@@ -1,1 +1,8 @@
-alias creator="python ~/Documents/Projects/Project-Automator/prjCreator.py"
+#!/bin/bash
+#
+# $1 -> folder name
+# $2 -> project name
+function creator() {
+    python ~/Documents/Projects/Project-Automator/prjCreator.py $1 $2
+    cd $1
+}
