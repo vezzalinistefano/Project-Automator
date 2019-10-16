dc60a000337a4e6b7b81c0ef78200f6a9108e976
Vezzalini Stefano <stefano@MacBook-Air-di-Vezzalini.local>
2019-10-16 11:47:43 +0200

    Added git init

--- prjCreator.py
+++ prjCreator.py
@@ -8,7 +8,9 @@ def creator (args):
     # than create the README.md file
     sp.call(["mkdir", args[1]])
     sp.call(["touch", args[1] + "/README.md"])
-    
+    sp.call(["git", "init", args[1] + "/"])
+
+
 
 if __name__ == "__main__":
     creator(sys.argv)
